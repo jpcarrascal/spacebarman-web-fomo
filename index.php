4e9e273eeb3a6dc0b7234019dd8d97e95fa20cbd
JP Carrascal <juanpcarrascal@gmail.com>
2020-08-09 21:27:00 +0200

    Getting there...

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     <nav>
     <ul>
         <li class="bar_item media" text="bio"  id="music">
-        <a href="https://open.spotify.com/artist/1o8zzq05yXKrsdQUrMzZdc?si=qEi8unstQ5qMCWFrdX2LAA" target=_blank>
+        <a href="bio/" target=_blank>
             Bio
         </a>
         </li>
@@ -45,7 +45,7 @@
         </li>
 
         <li class="bar_item media" text="subscribe" id="photos">
-        <a href="http://eepurl.com/dnDDUH" target=_blank>
+        <a href="https://spacebarman.us12.list-manage.com/subscribe?u=08fa2b83e7de09bfd19384dee&id=08f0d07b1e" target=_blank>
             Subscribe
         </a>
         </li>
@@ -59,36 +59,30 @@
         </li>
     </ul>
     </nav>
-
 </div>
 
 
 
 <div id="bg" class="bg">    
     
-    <div id="middle">
+    <div id="container">
         <div class="middle-text">
-            FOMO - New EP
         </div>
         
         <div class="middle-text">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/eqP2j7pND_w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
-
-        <div class="middle-text follow-icon-container">
-            <a href="https://www.instagram.com/spacebarman/" target=_blank>
-                <div class="follow-icon follow-ins" src="images/instagram-gold.png"></div>
-            </a>
-            <a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fspacebarman.com%2F&region=follow_link&screen_name=spacebarman&tw_p=followbutton&variant=2.0" target=_blank>
-                <div class="follow-icon follow-twi" src="images/twitter-gold.png"></div>
-            </a>
-            <a href="https://facebook.com/spacebarman" target=_blank>
-                <div class="follow-icon follow-fbk" style="width:28px;height:28px;" src="images/facebook-gold.png"></div>
-            </a>
-            <a href="https://spacebarman.us12.list-manage.com/subscribe?u=08fa2b83e7de09bfd19384dee&id=08f0d07b1e" target=_blank>
-                <div class="follow-icon follow-sub" src="images/mail-gold.png"></div>
-            </a>
+        <div class="listen-menu">
+            Listen
+            <div class="listen-dropdown">
+                <div class="play-service"><img class="service-logo" src="images/logo-spotify.png" /></div>
+                <div class="play-service"><img class="service-logo" src="images/logo-youtubemusic.png" /></div>
+                <div class="play-service"><img class="service-logo" src="images/logo-applemusic.png" /></div>
+                <div class="play-service"><img class="service-logo" src="images/logo-deezer.png" /></div>
+                <div class="play-service"><img class="service-logo" src="images/logo-amazon.png" /></div>
+                <div class="play-service"><img class="service-logo" src="images/logo-soundcloud.png" /></div>
+            </div>
             
         </div>
 
@@ -98,7 +92,7 @@
 
 
 
-<img src="images/city.png" class="fix_bottom" />
+
 
 </body>
 </html>
