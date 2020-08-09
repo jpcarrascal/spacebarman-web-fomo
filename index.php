775602cb6648c129c82a87a6f1a2aee900c0105c
JP Carrascal <juanpcarrascal@gmail.com>
2020-08-09 18:59:47 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-15242862-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-15242862-2');
+    </script>
+
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width,user-scalable =no" />
+    <meta name="description" content="Spacebarman is an electronic rock band from Barcelona.">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    <title>Spacebarman</title>
+
+
+    <link rel="stylesheet" href="https://use.typekit.net/oov2wcw.css">
+    <link href="reset.css" type="text/css" rel="stylesheet"/>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+
+
+
+<body>
+
+<div id="top_bar" class="top_bar">
+    <nav>
+    <ul>
+        <li class="bar_item media" text="bio"  id="music">
+        <a href="https://open.spotify.com/artist/1o8zzq05yXKrsdQUrMzZdc?si=qEi8unstQ5qMCWFrdX2LAA" target=_blank>
+            Bio
+        </a>
+        </li>
+    
+        <li class="bar_item media" text="video" id="videos">
+        <a href="https://www.youtube.com/user/spacebarmanvideo" target=_blank>
+            Video
+        </a>
+        </li>
+
+        <li class="bar_item media" text="subscribe" id="photos">
+        <a href="http://eepurl.com/dnDDUH" target=_blank>
+            Subscribe
+        </a>
+        </li>
+  
+        <li class="bar_item" id="connect">     
+            Follow
+            <div class="submenu">
+                <a href="https://www.instagram.com/spacebarman/" target=_blank><div class="bar_subitem">Instagram</div></a>
+                <a href="https://facebook.com/spacebarman" target=_blank><div class="bar_subitem">Facebook</div></a>
+                <a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fspacebarman.com%2F&region=follow_link&screen_name=spacebarman&tw_p=followbutton&variant=2.0" target=_blank><div class="bar_subitem">Twitter</div></a>
+        </li>
+    </ul>
+    </nav>
+
+</div>
+
+
+
+<div id="bg" class="bg">    
+    
+    <div id="middle">
+        <div class="middle-text">
+            FOMO - New EP
+        </div>
+        
+        <div class="middle-text">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/eqP2j7pND_w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+
+        <div class="middle-text follow-icon-container">
+            <a href="https://www.instagram.com/spacebarman/" target=_blank>
+                <div class="follow-icon follow-ins" src="images/instagram-gold.png"></div>
+            </a>
+            <a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fspacebarman.com%2F&region=follow_link&screen_name=spacebarman&tw_p=followbutton&variant=2.0" target=_blank>
+                <div class="follow-icon follow-twi" src="images/twitter-gold.png"></div>
+            </a>
+            <a href="https://facebook.com/spacebarman" target=_blank>
+                <div class="follow-icon follow-fbk" style="width:28px;height:28px;" src="images/facebook-gold.png"></div>
+            </a>
+            <a href="https://spacebarman.us12.list-manage.com/subscribe?u=08fa2b83e7de09bfd19384dee&id=08f0d07b1e" target=_blank>
+                <div class="follow-icon follow-sub" src="images/mail-gold.png"></div>
+            </a>
+            
+        </div>
+
+    </div>
+
+</div>
+
+
+
+<img src="images/city.png" class="fix_bottom" />
+
+</body>
+</html>
